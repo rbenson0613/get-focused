957c0a7c0b453ddcb2ad90fa5c58a22d1b7b8043
rbenson <ronaldebenson@gmail.com>
2025-10-11 15:13:10 -0400

    corrected build errors and added internet permission for mobile app

--- wear/build.gradle.kts
+++ wear/build.gradle.kts
@@ -44,6 +44,14 @@ android {
     buildFeatures {
         compose = true
     }
+    packaging {
+        resources.excludes.add("META-INF/LICENSE")
+        resources.excludes.add("META-INF/LICENSE.txt")
+        resources.excludes.add("META-INF/NOTICE")
+        resources.excludes.add("META-INF/NOTICE.txt")
+        resources.excludes.add("META-INF/DEPENDENCIES")
+        resources.excludes.add("META-INF/INDEX.LIST")
+    }
 }
 
 dependencies {
@@ -68,15 +76,7 @@ dependencies {
     implementation(libs.play.services.auth)
     implementation(libs.okhttp)
     implementation(libs.gson)
-    implementation(libs.google.api.client)
-    implementation(libs.google.http.client.android)
-    implementation(libs.google.http.client.gson)
-    implementation(libs.google.api.services.calendar)
-    implementation(libs.google.api.client.googleapis.extensions) {
-        exclude(group = "com.google.api.client")
-        exclude(group = "com.google.http-client")
-        exclude(group = "com.google.oauth-client")
-    }
+    implementation(libs.bundles.google.api)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
